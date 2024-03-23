8d2d1bb68740b55dc8aa94a55e0e4480430e99cf
oscar-monsalve <oscarmonsalvec5@gmail.com>
2024-03-23 13:49:50 -0500

    update

--- four_bar_linkage/main.py
+++ four_bar_linkage/main.py
@@ -3,27 +3,35 @@ import position as pos
 
 # four-bar linkage data
 theta_2 = 40  # degrees
-a = 40
+a = 60
 b = 120
 c = 80
 d = 100
 
 
 def main() -> None:
-    k1, k2, k3, k4, k5 = pos.k_constants(a, b, c, d)
-    A, B, C, D, F, E = pos.A_constants(theta_2, k1, k2, k3, k4, k5)
-    theta_3_1, theta_4_1, transmission_angle_open = pos.position_open(A, B, C, D, F, E)
-    theta_3_2, theta_4_2, transmission_angle_crossed = pos.position_crossed(A, B, C, D, F, E)
+    is_grashof = pos.is_grashof(a, b, c, d)
 
-    print("Open configuration:")
-    print(f"Theta_3_1 is: {theta_3_1: .2f} °")
-    print(f"Theta_4_1 is: {theta_4_1: .2f} °\n")
-    print(f"The transmission angle is: {transmission_angle_open: .2f}")
+    if is_grashof is True:
 
-    print("Crossed configuration:")
-    print(f"Theta_3_2 is: {theta_3_2: .2f} °")
-    print(f"Theta_4_2 is: {theta_4_2: .2f} °\n")
-    print(f"The transmission angle is: {transmission_angle_crossed: .2f}")
+        k1, k2, k3, k4, k5 = pos.k_constants(a, b, c, d)
+        A, B, C, D, F, E = pos.A_constants(theta_2, k1, k2, k3, k4, k5)
+        theta_3_1, theta_4_1, transmission_angle_open = pos.position_open(A, B, C, D, F, E)
+        theta_3_2, theta_4_2, transmission_angle_crossed = pos.position_crossed(A, B, C, D, F, E)
+
+        print("Open configuration:")
+        print(f"Theta_3_1 is: {theta_3_1: .2f} °")
+        print(f"Theta_4_1 is: {theta_4_1: .2f} °\n")
+        print(f"The transmission angle is: {transmission_angle_open: .2f}")
+
+        print("Crossed configuration:")
+        print(f"Theta_3_2 is: {theta_3_2: .2f} °")
+        print(f"Theta_4_2 is: {theta_4_2: .2f} °\n")
+        print(f"The transmission angle is: {transmission_angle_crossed: .2f}")
+
+    if is_grashof is False:
+        print("Therefore, no link can describe a full rotation.")
+        print("Please change the lengths of your linkages so the Grashof's law is satified")
 
 
 if __name__ == "__main__":
--- four_bar_linkage/position.py
+++ four_bar_linkage/position.py
@@ -1,9 +1,24 @@
 import numpy as np
 
 
-def check_grashof(a: float, b: float, c: float, d: float) -> bool:
+def is_grashof(a: float, b: float, c: float, d: float) -> bool:
     min_len = min(a, b, c, d)
     max_len = max(a, b, c, d)
+    other_links_sum = a + b + c + d - min_len - max_len
+    print("Does the linkage satify the Grashof's law?")
+
+    if min_len + max_len < other_links_sum:
+        print("""Yes, it is a class I Grashof mechanism. Meaning at least one of the linkages will describe
+        a full rotation.\n""")
+        return True
+    if min_len + max_len == other_links_sum:
+        print("""Yes, it is a class III Grashof mechanism. Meaning at least one of the linkages will describe
+        a full rotation and colineal positions will occur.\n""")
+        return True
+    if min_len + max_len > other_links_sum:
+        print("No, is it a class II non-Grashof mechanism. Meaning no linkage will describe a full rotation.\n")
+        return False
+
 
 def k_constants(a: float, b: float, c: float, d: float) -> float:
     """Returns the K_n constants. Its arguments are the mechanisms' lengths a, b, c, d."""
