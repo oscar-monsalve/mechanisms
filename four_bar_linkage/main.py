a39368cf274f964bf841546a3e250e087751661f
oscar-monsalve <oscarmonsalvec5@gmail.com>
2024-03-22 20:21:22 -0500

    added the position calculations

--- four_bar_linkage/main.py
+++ four_bar_linkage/main.py
@@ -1,8 +1,30 @@
 # import matplotlib.pyplot as plt
-# import position as pos
+import position as pos
 
 # four-bar linkage data
+theta_2 = 40  # degrees
 a = 40
 b = 120
 c = 80
 d = 100
+
+
+def main() -> None:
+    k1, k2, k3, k4, k5 = pos.k_constants(a, b, c, d)
+    A, B, C, D, F, E = pos.A_constants(theta_2, k1, k2, k3, k4, k5)
+    theta_3_1, theta_4_1, transmission_angle_open = pos.position_open(A, B, C, D, F, E)
+    theta_3_2, theta_4_2, transmission_angle_crossed = pos.position_crossed(A, B, C, D, F, E)
+
+    print("Open configuration:")
+    print(f"Theta_3_1 is: {theta_3_1: .2f} °")
+    print(f"Theta_4_1 is: {theta_4_1: .2f} °\n")
+    print(f"The transmission angle is: {transmission_angle_open: .2f}")
+
+    print("Crossed configuration:")
+    print(f"Theta_3_2 is: {theta_3_2: .2f} °")
+    print(f"Theta_4_2 is: {theta_4_2: .2f} °\n")
+    print(f"The transmission angle is: {transmission_angle_crossed: .2f}")
+
+
+if __name__ == "__main__":
+    main()
--- four_bar_linkage/position.py
+++ four_bar_linkage/position.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def k_constants(a, b, c, d):
+def k_constants(a: float, b: float, c: float, d: float) -> float:
     """Returns the K_n constants. Its arguments are the mechanisms' lengths a, b, c, d."""
 
     k1 = d / a
@@ -13,22 +13,35 @@ def k_constants(a, b, c, d):
     return k1, k2, k3, k4, k5
 
 
-def A_constants(theta_2):
-    """Returns the capital letters variables. It's arguments are theta_2."""
+def A_constants(theta_2: float, k1, k2, k3, k4, k5) -> float:
+    """Returns the capital letters variables. It's arguments are theta_2, k1, k2, k3, k4, k5"""
 
-    theta_2_deg = np.deg2rad(theta_2)
-    kn = k_constants()
-    A = np.cos(theta_2_deg) - kn[0] - kn[1]*np.cos(theta_2_deg) + kn[2]
-    B = -2 * np.sin(theta_2_deg)
+    theta_2_rad = np.deg2rad(theta_2)
+    A = np.cos(theta_2_rad) - k1 - (k2 * np.cos(theta_2_rad)) + k3
+    B = -2 * np.sin(theta_2_rad)
+    C = k1 - (k2 + 1) * np.cos(theta_2_rad) + k3
+    D = np.cos(theta_2_rad) - k1 + (k4 * np.cos(theta_2_rad)) + k5
+    E = -2 * np.sin(theta_2_rad)
+    F = k1 + (k4 - 1) * np.cos(theta_2_rad) + k5
 
-    return A, B
+    return A, B, C, D, E, F
 
 
-# four-bar linkage data
-theta_2 = 40  # degrees
-a = 40
-b = 120
-c = 80
-d = 100
+def position_open(A, B, C, D, E, F):
+    """Returns the position of the mechanism (theta_3_1 & theta_4_) for the open configuration"""
 
-print(k_constants(a, b, c, d))
+    theta_3_1 = 2 * np.arctan((-E - np.sqrt(E**2 - 4 * D * F)) / (2 * D))
+    theta_4_1 = 2 * np.arctan((-B - np.sqrt(B**2 - 4 * A * C)) / (2 * A))
+    transmission_angle = theta_4_1 - theta_3_1
+
+    return np.rad2deg(theta_3_1), np.rad2deg(theta_4_1), np.rad2deg(transmission_angle)
+
+
+def position_crossed(A, B, C, D, E, F):
+    """Returns the position of the mechanism (theta_3_2 & theta_4_2) for the crossed configuration"""
+
+    theta_3_2 = 2 * np.arctan((-E + np.sqrt(E**2 - 4 * D * F)) / (2 * D))
+    theta_4_2 = 2 * np.arctan((-B + np.sqrt(B**2 - 4 * A * C)) / (2 * A))
+    transmission_angle = theta_3_2 - theta_4_2
+
+    return np.rad2deg(theta_3_2), np.rad2deg(theta_4_2), np.rad2deg(transmission_angle)
