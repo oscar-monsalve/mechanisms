69239a4e924f6f02d08afc97ebb47c08ec30d7a4
oscar-monsalve <oscarmonsalvec5@gmail.com>
2024-03-30 14:52:55 -0500

    update

--- four_bar_linkage/main.py
+++ four_bar_linkage/main.py
@@ -1,4 +1,5 @@
-# import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
 import position as pos
 import grashof as grashof
 import velocity as vel
@@ -12,7 +13,8 @@ def main() -> None:
     b = 120
     c = 80
     d = 100
-    theta_2 = 40  # degrees
+    input_theta_2 = 40  # Input link position to print specific position results.
+    theta_2 = np.linspace(0, 360, 360)  # degrees
     omega_2 = 25  # rad/s
     alpha_2 = 5  # rad/s^2
 
@@ -39,36 +41,45 @@ def main() -> None:
         print("---------------------------------------")
         print("Position:")
         print("Open configuration:")
-        print(f"Theta_3_1 is: {theta_3_1: .2f} °")
-        print(f"Theta_4_1 is: {theta_4_1: .2f} °")
-        print(f"The transmission angle is: {transmission_angle_open: .2f} °\n")
+        print(f"Theta_3_1 is: {theta_3_1[input_theta_2]: .2f} °")
+        print(f"Theta_4_1 is: {theta_4_1[input_theta_2]: .2f} °")
+        print(f"The transmission angle is: {transmission_angle_open[input_theta_2]: .2f} °\n")
 
         print("Crossed configuration:")
-        print(f"Theta_3_2 is: {theta_3_2: .2f} °")
-        print(f"Theta_4_2 is: {theta_4_2: .2f} °")
-        print(f"The transmission angle is: {transmission_angle_crossed: .2f} °")
+        print(f"Theta_3_2 is: {theta_3_2[input_theta_2]: .2f} °")
+        print(f"Theta_4_2 is: {theta_4_2[input_theta_2]: .2f} °")
+        print(f"The transmission angle is: {transmission_angle_crossed[input_theta_2]: .2f} °")
         print("---------------------------------------\n")
 
         # Velocity print
         print("Velocity:")
         print("Open configuration:")
-        print(f"Omega_3_1 is: {omega_3_1: .2f} rad/s")
-        print(f"Omega_3_1: {omega_4_1: .2f} rad/s")
+        print(f"Omega_3_1 is: {omega_3_1[input_theta_2]: .2f} rad/s")
+        print(f"Omega_3_1: {omega_4_1[input_theta_2]: .2f} rad/s")
         print(f"V_A is: {v_a: .2f} length/s")
-        print(f"V_B_A is: {v_b_a: .2f} length/s")
-        print(f"V_B is: {v_b: .2f} length/s\n")
+        print(f"V_B_A is: {v_b_a[input_theta_2]: .2f} length/s")
+        print(f"V_B is: {v_b[input_theta_2]: .2f} length/s\n")
 
         print("crossed configuration:")
-        print(f"omega_3_2 is: {omega_3_2: .2f} rad/s")
-        print(f"omega_3_1 is: {omega_4_2: .2f} rad/s")
+        print(f"omega_3_2 is: {omega_3_2[input_theta_2]: .2f} rad/s")
+        print(f"omega_3_1 is: {omega_4_2[input_theta_2]: .2f} rad/s")
         print("---------------------------------------\n")
 
         # Acceleration print
         print("Acceleration:")
         print("Open configuration:")
-        print(f"Alpha_3_1 is: {alpha_3_1: .2f} rad/s^2")
-        print(f"Alpha_4_1: {alpha_4_1: .2f} rad/s^2")
+        print(f"Alpha_3_1 is: {alpha_3_1[input_theta_2]: .2f} rad/s^2")
+        print(f"Alpha_4_1: {alpha_4_1[input_theta_2]: .2f} rad/s^2")
         print("---------------------------------------\n")
+
+        plt.plot(theta_2, theta_3_1)
+        plt.plot(theta_2, theta_4_1)
+        plt.plot(theta_2, transmission_angle_open)
+        plt.legend([r"$\theta_3$", r"$\theta_4$", r"$\mu$"])
+        plt.title("Four-bar linkage position (open configuration)")
+        plt.xlabel(r"$\theta_2\; (°)$")
+        plt.ylabel("Angular position (°)")
+        plt.show()
 
     if is_grashof is False:
         print("Therefore, no link can describe a full rotation.")
